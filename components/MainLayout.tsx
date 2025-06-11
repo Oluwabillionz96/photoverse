c1fbbaa71af8571d51cc1df3672fc0f2acd02cca
oluwabillionz96 <goodluckreuben96@gmail.com>
2025-06-11 15:52:23 +0100

    unstyled modal added

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -1,35 +1,45 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SideNav from "./SideNav";
 import { motion } from "framer-motion";
 import useScreenSize from "@/hooks/useScreenSize";
+import AuthenticationModal from "./AuthenticationModal";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [collapsed, setCollapsed] = useState(false);
   const isMobile = useScreenSize();
+
+  useEffect(() => {
+    setIsAuthenticated(false);
+  }, []);
   return (
-    <motion.main
-      animate={{
-        marginLeft: isMobile ? "0" : collapsed ? "5rem" : "17.5rem",
-      }}
-      transition={{ duration: 0.3, ease: "easeInOut" }}
-    >
-      <motion.header className={`bg-[#141414]`}>
-        <Link href={"/"}>
-          <Image
-            width={100}
-            height={100}
-            src={"/photoverse-logo.png"}
-            alt="logo"
-            className="block mx-auto"
-          />
-        </Link>
-      </motion.header>
-      <SideNav collapsed={collapsed} setCollapsed={setCollapsed} />
-      {children}
-    </motion.main>
+    <>
+      <motion.main
+        animate={{
+          marginLeft: isMobile ? "0" : collapsed ? "5rem" : "17.5rem",
+        }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+        className="relative"
+      >
+        {!isAuthenticated && <AuthenticationModal />}
+        <motion.header className={`bg-[#141414]`}>
+          <Link href={"/"}>
+            <Image
+              width={100}
+              height={100}
+              src={"/photoverse-logo.png"}
+              alt="logo"
+              className="block mx-auto"
+            />
+          </Link>
+        </motion.header>
+        <SideNav collapsed={collapsed} setCollapsed={setCollapsed} />
+        {children}
+      </motion.main>
+    </>
   );
 };
 
